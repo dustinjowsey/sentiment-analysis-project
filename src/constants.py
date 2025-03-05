17b92af8ce504fbce3b99644a19406f9e83170bd
Dustin Jowsey <73204316+dustinjowsey@users.noreply.github.com>
2025-03-05 00:17:33 -0500

    general constants

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+#path to raw data
+PATH_DATA_RAW = "../data/raw/"
+#path to preprocessed data
+PATH_DATA_PROCESSED = "../data/preprocessed/"
+#path to data that has undergone bag of words conversion
+PATH_DATA_BOW = "../data/bag_of_words/"
