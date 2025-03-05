48f333cd8e206ac45826011486e9933c342f116b
Dustin Jowsey <73204316+dustinjowsey@users.noreply.github.com>
2025-03-05 00:16:55 -0500

    Cinfig to load src files into notebooks

---
+++ notebooks/config.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+#append the root folder to path
+root = os.path.dirname(__file__)
+if root not in sys.path:
+    sys.path.append(root)
+
+#Append src to path so we can use the modules in src in our notebooks easier
+#Just include config in your notebook
+src = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+if src not in sys.path:
+    sys.path.append(src)
