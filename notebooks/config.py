52a66303d30840277e4a423bed7cac25ea6ef1d5
dustin jowsey <dustinjowsey@hotmail.com>
2025-03-08 16:04:06 -0800

    Fixed path errors

--- notebooks/config.py
+++ notebooks/config.py
@@ -2,12 +2,12 @@ import sys
 import os
 
 #append the root folder to path
-root = os.path.dirname(__file__)
+root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 if root not in sys.path:
     sys.path.append(root)
-
+    
 #Append src to path so we can use the modules in src in our notebooks easier
 #Just include config in your notebook
-src = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+src = os.path.abspath(os.path.join(root, 'src'))
 if src not in sys.path:
     sys.path.append(src)
