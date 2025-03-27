95af0c12b8f1503d355fce4bccfd8a47191328ce
dustin jowsey <dustinjowsey@hotmail.com>
2025-03-26 18:52:21 -0700

    Added path to word embeddings datasets

--- src/constants.py
+++ src/constants.py
@@ -4,5 +4,7 @@ PATH_DATA_RAW = "../data/raw/"
 PATH_DATA_PROCESSED = "../data/preprocessed/"
 #path to data that has undergone bag of words conversion
 PATH_DATA_BOW = "../data/bag_of_words/"
+#path to word embedding files
+PATH_TO_WE = "../data/word_embeddings/"
 
 SEED=474
